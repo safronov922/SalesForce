24fcd1f2be9efbb28cc3127078af431ed2c7d86a
safronov922 <safronov922@yandex.ru>
2022-04-14 15:26:49 +0300

    HW16 Allure reports

--- src/test/java/tests/AccountTest.java
+++ src/test/java/tests/AccountTest.java
@@ -1,14 +1,21 @@
 package tests;
 
+import io.qameta.allure.Description;
 import models.SalesForceAccountModel;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.AccountsPage;
 import pages.CreateAccountPage;
 import pages.LoginPage;
 import testdata.GetNewAccountModel;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class AccountTest extends BaseTest {
 
@@ -18,7 +25,8 @@ public class AccountTest extends BaseTest {
 
 
     @Test
-    public void createAccountTest() {
+    @Description("Check that account in SaleForce created")
+    public void createAccountTest() throws IOException {
         LOGGER.info("createAccountTest started");
         LoginPage loginPage = new LoginPage(driver);
         LOGGER.info("open page" + loginPage.getClass().getName());
@@ -34,6 +42,9 @@ public class AccountTest extends BaseTest {
         LOGGER.info("Attempt to create account with name " + salesForceAccountModel.getAccountName());
         createAccountPage.saveNewAccountForm(salesForceAccountModel);
         boolean success = createAccountPage.displaedSuccessMessage();
+        TakesScreenshot screenshot = ((TakesScreenshot) driver);
+        byte[] sourceFile = screenshot.getScreenshotAs(OutputType.BYTES);
+        Files.write(Paths.get("src/test/resources/screenshot1.png"),sourceFile);
         Assert.assertTrue(success, "Account don't create");
     }
 
---
+++ src/test/java/utilites/TestListener.java
@@ -0,0 +1,53 @@
+package utilites;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import tests.BaseTest;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class TestListener implements ITestListener {
+    @Override
+    public void onTestStart(ITestResult iTestResult) {
+
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult iTestResult) {
+
+    }
+
+    @Override
+    public void onTestFailure(ITestResult iTestResult) {
+        Object currentClass = iTestResult.getInstance();
+        WebDriver webDriver = ((BaseTest) currentClass).getDriver();
+        AllureService allureService = new AllureService();
+        allureService.takeScreenShot(webDriver);
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult iTestResult) {
+
+    }
+
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
+
+    }
+
+    @Override
+    public void onStart(ITestContext iTestContext) {
+
+    }
+
+    @Override
+    public void onFinish(ITestContext iTestContext) {
+
+    }
+}
